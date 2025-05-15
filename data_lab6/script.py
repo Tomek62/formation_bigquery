60ba2a85507a11cf5d8c355b231f57ec330ab7b5
ihab abadi <ihab@utopios.net>
2025-05-15 14:45:43 +0200

    avec source lab6

---
+++ data_lab6/script.py
@@ -0,0 +1,29 @@
+from google.cloud import bigquery
+import functions_framework
+
+@functions_framework.http
+def generate_report(request):
+    client = bigquery.Client()
+    query = """
+    SELECT
+      flight_date AS date,
+      carrier,
+      delay_cause AS cause,
+      SUM(num_delays) AS num_delays
+    FROM
+      `bigquery-public-data.faa.us_delay_causes`
+    WHERE
+      flight_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY)
+    GROUP BY
+      date, carrier, cause
+    HAVING
+      num_delays > 15
+    """
+
+    try:
+        job_config = bigquery.QueryJobConfig(destination="`your_project_id.your_dataset.daily_flight_report`", write_disposition="WRITE_TRUNCATE")
+        query_job = client.query(query, job_config=job_config)
+        query_job.result()
+        return "✅ Rapport généré avec succès", 200
+    except Exception as e:
+        return f"❌ Erreur: {str(e)}", 500
